d8d8ff6c4c7236d9eb3221630d7b908a0f4f418c
nikunj ladva <nikunjladva245@gmail.com>
2021-10-22 19:27:17 +0530

    after add navigation component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
+
+const App = () => {
+  const [darkTheme, setDarktheme] = useState(false);
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
